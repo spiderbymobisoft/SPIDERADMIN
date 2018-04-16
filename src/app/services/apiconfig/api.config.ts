38dad2abf20a86c92454e4e5eae4d0ef09b7a3b7
kolagrey <kolagrey@gmail.com>
2018-04-16 20:25:59 +0100

    Update by Grey

--- src/app/services/apiconfig/api.config.ts
+++ src/app/services/apiconfig/api.config.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 export class APIConfig { 
     config = {
         appVersion : "1.0.0",
-        apiURL :  "https://api.spider.com.ng",
+        apiURL :  "https://api.spider.com.ng/",
         authorization : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfYnNvbnR5cGUiOiJPYmplY3RJRCIsImlkIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjpbODksMjQ5LDE2NCwxNywyMzIsMzAsMjI4LDExLDUyLDE4MiwyMzIsMjA4XX0sImlhdCI6MTUwOTUzMjY5MX0.ghQBHtQdOP63jqP7bysGrB9N1sBZdmVh8H5RdOQXzdw"
     }
     getConfig(): Object {
